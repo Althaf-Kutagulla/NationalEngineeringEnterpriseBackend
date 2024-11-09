42e34c372a1a9d65425e087c6d78dda24ea8a6e4
Althaf Kutagulla <kutagullaalthaf12@gmail.com>
2024-11-09 16:40:51 +0530

    intial commit

---
+++ src/main/java/org/nationalengineering/NationalEngineeringEnterpriseApplication.java
@@ -0,0 +1,13 @@
+package org.nationalengineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NationalEngineeringEnterpriseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NationalEngineeringEnterpriseApplication.class, args);
+    }
+
+}
