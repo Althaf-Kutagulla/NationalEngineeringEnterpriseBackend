f731afef891135c04de319c9ff87d48cc1534b48
Althaf Kutagulla <kutagullaalthaf12@gmail.com>
2024-11-12 14:27:47 +0530

    Created CustomerRepository for database operations

---
+++ src/main/java/org/nationalengineering/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package org.nationalengineering.repository;
+
+import org.nationalengineering.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Integer> {
+}
