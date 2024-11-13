8f0f4af933a1490a56968633b52a3e19abb8c306
Althaf Kutagulla <kutagullaalthaf12@gmail.com>
2024-11-13 10:25:51 +0530

    WorkerMapper.class to convert Worker objects

---
+++ src/main/java/org/nationalengineering/mappers/WorkerMapper.java
@@ -0,0 +1,39 @@
+package org.nationalengineering.mappers;
+
+import org.nationalengineering.entity.Worker;
+import org.nationalengineering.records.WorkerRequest;
+import org.nationalengineering.records.WorkerResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class WorkerMapper {
+    public Worker toWorker(WorkerRequest workerRequest){
+        return Worker.builder()
+                .firstName(workerRequest.firstName())
+                .lastName(workerRequest.lastName())
+                .phoneNumber(workerRequest.phoneNumber())
+                .alterPhoneNumber(workerRequest.alterPhoneNumber())
+                .town(workerRequest.town())
+                .village(workerRequest.village())
+                .address(workerRequest.address())
+                .workingStatus(workerRequest.workingStatus())
+                .build();
+    }
+
+    public WorkerResponse toWorkerResponse(Worker worker) {
+        return new WorkerResponse(
+                worker.getFirstName(),
+                worker.getLastName(),
+                worker.getPhoneNumber(),
+                worker.getAlterPhoneNumber(),
+                worker.getTown(),
+                worker.getVillage(),
+                worker.getAddress(),
+                worker.getWorkingStatus(),
+                worker.getJoinedDate(),
+                worker.getLeftDate()
+        );
+    }
+}
