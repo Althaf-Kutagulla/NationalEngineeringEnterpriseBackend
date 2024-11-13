ba2feaaad22a9707a3b69b09740c42bbe61e5954
Althaf Kutagulla <kutagullaalthaf12@gmail.com>
2024-11-13 10:22:44 +0530

    WokerController.class to hadle worker requests

---
+++ src/main/java/org/nationalengineering/controllers/WorkerController.java
@@ -0,0 +1,45 @@
+package org.nationalengineering.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.nationalengineering.records.WorkerResponse;
+import org.nationalengineering.records.WorkerRequest;
+import org.nationalengineering.service.WorkerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/workers")
+public class WorkerController {
+
+    private final WorkerService workerService;
+
+    @PostMapping("/create")
+    public ResponseEntity<Integer> createWorker(WorkerRequest workerRequest){
+        return new ResponseEntity<>(workerService.createWorker(workerRequest), HttpStatus.CREATED);
+    }
+
+    @GetMapping("{workerId}")
+    public ResponseEntity<WorkerResponse> getWorkerById(@PathVariable("workerId") Integer workerId){
+        return new ResponseEntity<>(workerService.getWorkerById(workerId),HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<WorkerResponse>> getAllWorkers(){
+        return new ResponseEntity<>(workerService.getAllWorkers(),HttpStatus.OK);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<Boolean> updateWorker(WorkerRequest workerRequest){
+        return new ResponseEntity<>(workerService.updateWorker(workerRequest),HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{workerId}")
+    public ResponseEntity<Boolean> deleteWorker(@PathVariable("workerId") Integer workerId){
+        return new ResponseEntity<>(workerService.deleteWorker(workerId),HttpStatus.OK);
+    }
+
+}
