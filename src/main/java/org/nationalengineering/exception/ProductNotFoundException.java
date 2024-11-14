2a2b8e51a57cdafdb05e1c65ba505d2269ef98c4
Althaf Kutagulla <kutagullaalthaf12@gmail.com>
2024-11-14 18:10:43 +0530

    ProductNotFoundException.class

---
+++ src/main/java/org/nationalengineering/exception/ProductNotFoundException.java
@@ -0,0 +1,7 @@
+package org.nationalengineering.exception;
+
+public class ProductNotFoundException extends RuntimeException{
+    public ProductNotFoundException(String message){
+        super(message);
+    }
+}
