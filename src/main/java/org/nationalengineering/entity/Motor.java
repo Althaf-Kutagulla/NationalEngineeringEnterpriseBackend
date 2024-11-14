d506d94c335d64e15bed4e3176dad5c1e74b94be
Althaf Kutagulla <kutagullaalthaf12@gmail.com>
2024-11-14 14:18:27 +0530

    Motor Entity

--- src/main/java/org/nationalengineering/entity/Motor.java
+++ src/main/java/org/nationalengineering/entity/Motor.java
@@ -1,6 +1,18 @@
 package org.nationalengineering.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "motors")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Motor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String name;
     private String model;
